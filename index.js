d842230afe20bbbe0898bf6ea0501c2c7c8a30b0
Gwangwon Jung <rhdk5148@gmail.com>
2025-02-03 23:07:42 +0900

    라벨로 이미지 변경

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
-// 오일
-const oils = [
-    {id:"0",name:"없음",path:""},
-    {id:"1",name:"아보카도 엑스트라버진",path:"./images/oil/아보카도엑스트라버진.png"},
-    {id:"2",name:"올리브 엑스트라버진",path:"./images/oil/올리브엑스트라버진.png"},
-    {id:"3",name:"올리브 포마스",path:"./images/oil/올리브포마스.png"},
-    {id:"4",name:"트러플 오일",path:"./images/oil/트러플오일.png"},
-    {id:"5",name:"해바라기",path:"./images/oil/해바라기.png"},
-]
-
-const oilSelector = document.querySelectorAll(".oil-selector");
-const oilImgSelector = document.querySelectorAll(".oil-img-selector");
-
-oilSelector.forEach((selector,index)=>{
-    selector.innerHTML = oils.map(oil =>
-        `<option value="${oil.id}">${oil.name}`
-    ).join("");
-    selector.addEventListener('change',()=>{
-        const selectedOil = oils.find(oil => oil.id === selector.value);
-        oilImgSelector[index].src = selectedOil.path;
-        oilImgSelector[index].alt = selectedOil.name;
-    });
-});
-
 // 루땡
 const topSeals = [
     {id:"0",name:"없음",path:""},
@@ -49,41 +25,101 @@ topSealSelector.forEach((selector,index)=>{
 // 라벨
 const labels = [
     {id:"0",group:"없음",name:"없음",path:""},
-    {id:"1",group:"에스엠디",name:"아보카도",path:"./images/labels/에스엠디-아보카도.png"},
-    {id:"2",group:"올가유",name:"아보카도",path:"./images/labels/올가유-아보카도.png"},
-    {id:"3",group:"제이에스팜",name:"아보카도",path:"./images/labels/제이에스팜-아보카도.png"},
-    {id:"4",group:"태웅(홈쇼핑)",name:"아보카도",path:"./images/labels/태웅(홈쇼핑)-아보카도.png"},
-    {id:"5",group:"파니몰",name:"아보카도",path:"./images/labels/파니몰-아보카도.png"},
+    {id:"1",group:"아칸",name:"아보카도 엑스트라버진",lpath:"",opath:"./images/oil/아칸-아보카도엑스트라버진.png"},
+    {id:"2",group:"에스엠디",name:"아보카도 엑스트라버진",lpath:"./images/labels/에스엠디-아보카도.png",opath:"./images/oil/에스엠디-아보카도엑스트라버진.png"},
+    {id:"3",group:"올가유",name:"아보카도 엑스트라버진",lpath:"./images/labels/올가유-아보카도.png",opath:"./images/oil/올가유-아보카도엑스트라버진.png"},
+    {id:"4",group:"제이에스팜",name:"아보카도 엑스트라버진",lpath:"./images/labels/제이에스팜-아보카도.png",opath:"./images/oil/제이에스팜-아보카도엑스트라버진.png"},
+    {id:"5",group:"태웅(홈쇼핑)",name:"아보카도 엑스트라버진",lpath:"./images/labels/태웅(홈쇼핑)-아보카도.png",opath:"./images/oil/태웅(홈쇼핑)-아보카도엑스트라버진.png"},
+    {id:"6",group:"파니몰",name:"아보카도 엑스트라버진",lpath:"./images/labels/파니몰-아보카도.png",opath:"./images/oil/파니몰-아보카도엑스트라버진.png"},
+    {id:"7",group:"아칸",name:"올리브 엑스트라버진",lpath:"",opath:"./images/oil/올리브엑스트라버진.png"},
+    {id:"8",group:"아칸",name:"올리브 포마스",lpath:"",opath:"./images/oil/올리브포마스.png"},
+    {id:"9",group:"아칸",name:"트러플 오일",lpath:"",opath:"./images/oil/트러플오일.png"},
+    {id:"10",group:"아칸",name:"해바라기",lpath:"",opath:"./images/oil/해바라기.png"},
 ];
+
+// 오일
+const oils = [
+    {id:"1",label:"아칸",name:"아보카도 엑스트라버진"},
+    {id:"2",label:"에스엠디",name:"아보카도 엑스트라버진"},
+    {id:"3",label:"올가유",name:"아보카도 엑스트라버진"},
+    {id:"4",label:"제이에스팜",name:"아보카도 엑스트라버진"},
+    {id:"5",label:"태웅(홈쇼핑)",name:"아보카도 엑스트라버진"},
+    {id:"6",label:"파니몰",name:"아보카도 엑스트라버진"},
+    {id:"7",label:"아칸",name:"올리브 엑스트라버진"},
+    {id:"8",label:"아칸",name:"올리브 포마스"},
+    {id:"9",label:"아칸",name:"트러플 오일"},
+    {id:"10",label:"아칸",name:"해바라기"},
+]
+
+const oils_list = [
+    {id:"0",name:"없음"},
+    {id:"1",name:"아보카도 엑스트라버진"},
+    {id:"2",name:"올리브 엑스트라버진"},
+    {id:"3",name:"올리브 포마스"},
+    {id:"4",name:"트러플 오일"},
+    {id:"5",name:"해바라기"},
+];
+const oilSelector = document.querySelectorAll(".oil-selector");
+const oilImgSelector = document.querySelectorAll(".oil-img-selector");
 
 const labelSelector = document.querySelectorAll(".label-selector");
 const labelImgSelector = document.querySelectorAll(".label-img-selector");
 
 // 그룹별로 데이터 정리
 const groupedLabels = labels.reduce((groups, label) => {
-    if (!groups[label.group]) {
-        groups[label.group] = [];
+    if (!groups[label.name]) {
+        groups[label.name] = [];
     }
-    groups[label.group].push(label);
+    groups[label.name].push(label);
     return groups;
 }, {});
 
-labelSelector.forEach((selector, index) => {
-    // 그룹별로 <optgroup>과 <option> 생성
-    selector.innerHTML = Object.keys(groupedLabels).map(group => `
-        <optgroup label="${group}">
-            ${groupedLabels[group].map(label => `
-                <option value="${label.id}">${label.name}</option>
-            `).join('')}
-        </optgroup>
-    `).join('');
 
-    // 선택 이벤트 처리
-    selector.addEventListener('change', () => {
-        const selectedLabel = labels.find(label => label.id === selector.value);
-        if (selectedLabel) {
-            labelImgSelector[index].src = selectedLabel.path;
-            labelImgSelector[index].alt = selectedLabel.name;
-        }
+oilSelector.forEach((selector,index)=>{
+    selector.innerHTML = oils_list.map(oil =>
+        `<option value="${oil.id}">${oil.name}`
+    ).join("");
+    selector.addEventListener('change',()=>{
+        const selectedOil = oils_list.find(oil => oil.id === selector.value).name;
+        
+        labelSelector.forEach((selector2,index2)=>{
+            // 필터링
+            const filteredLabels = groupedLabels[selectedOil] || [];
+            // 한개의 경우
+            if(filteredLabels.length === 1){
+                selector2.innerHTML = `<option value="${filteredLabels[0].group}">${filteredLabels[0].group}</option>`
+                const selectedLabel = labels.find(
+                    label => (label.name === selectedOil) && (label.group === selector2.value)
+                )
+                labelImgSelector[index2].src = selectedLabel.lpath;
+                labelImgSelector[index2].alt = selectedLabel.name;
+                oilImgSelector[index2].src = selectedLabel.opath;
+                oilImgSelector[index2].alt = selectedLabel.name;
+            }
+            else{
+                selector2.innerHTML = filteredLabels.map(label => `
+                    <option value="${label.group}">${label.group}</option>
+                `).join('');
+                // 기본값
+                const selectedLabel = labels.find(
+                    label => (label.name === selectedOil) && (label.group === selector2.value)
+                )
+                labelImgSelector[index2].src = selectedLabel.lpath;
+                labelImgSelector[index2].alt = selectedLabel.name;
+                oilImgSelector[index2].src = selectedLabel.opath;
+                oilImgSelector[index2].alt = selectedLabel.name;
+
+                selector2.addEventListener('change',()=>{
+                    // 그룹 체크, 이름 체크
+                    const selectedLabel = labels.find(
+                        label => (label.name === selectedOil) && (label.group === selector2.value)
+                    )
+                    labelImgSelector[index2].src = selectedLabel.lpath;
+                    labelImgSelector[index2].alt = selectedLabel.name;
+                    oilImgSelector[index2].src = selectedLabel.opath;
+                    oilImgSelector[index2].alt = selectedLabel.name;
+                })
+            }
+        });
     });
 });
